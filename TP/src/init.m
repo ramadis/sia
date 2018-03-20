ab4aea22623cb7c91e3f5aa8b1ae8180223a9b1a
ramadis <rramiro.o@hotmail.com>
2018-03-19 22:13:02 -0300

    A little bit more cleanup

--- TP/src/init.m
+++ TP/src/init.m
@@ -1,5 +1,6 @@
 addpath('../lib/jsonlab');
+rand('state',sum(100*clock)); % seed random state
 
-configuration = config.parse(config.read('../config.json'))
-%mlp = network.build(configuration);
+configuration = config.parse(config.read('../config.json'));
+mlp = network.build(configuration);
 %mlp.train(parse.input('../input/train.json'));
--- TP/src/network.m
+++ TP/src/network.m
@@ -4,37 +4,33 @@ endfunction
 
 function retval = setInitialWeights (weights, size) % TODO check function signature
   if (rows(weights) < 1)
-    retval = rand(size);
+    retval = 2 * rand(4,1) - 1; % random values within [-1;1]
     return
   endif
-
-  % THETA1 = 2*rand(2,3) - 1; 2 x 3 matrix within [-1;1]
 
   retval = weights;
 endfunction
 
 function net = build (config)
-  config
+
   weights = setInitialWeights(config.weights);
-  %{
   function trainretval = train (data)
     for epoch = 1:config.epochs % For each epoch
 
       for idxSample = 1:rows(data) % Iterate through each sample (Lets do it stochasticly, why not)
-        output[1] = data[idxSample];
+        output = [ data(idxSample) ];
 
         % Forward passing
         for idxLayer = 1:rows(config.layers)
-          layer = config.layers[idxLayer];
-          input[idxLayer] = [layer.bias; output];
-          weights[idxLayer] = config.weights[idxLayer];
-          output[idxLayer + 1] = layer.activation(weight * input);
+          layer = config.layers(idxLayer);
+          input = [ input [layer.bias; output] ];
+          weights = [ weights config.weights(idxLayer) ];
+          output = [ output layer.activation(weight * input)];
         end
 
         % Backpropagation
         for idxLayer = rows(config.layers):2
-          (weights[idxLayer - 1]' * delta[idxLayer]) .* (output[idxLayer] .* (1 - output[idxLayer]))(2:end);
-
+          %(weights[idxLayer - 1]' * delta[idxLayer]) .* (output[idxLayer] .* (1 - output[idxLayer]))(2:end);
         end
 
         % Here take place the optimizations (After k samples, or 1 epoch)
@@ -46,8 +42,7 @@ function net = build (config)
   function testretval = test (data)
     testretval = 1;
   endfunction
-  %}
 
-  net.train = train;
-  net.test = test;
+  %net = @train;
+  %net.test = @test;
 endfunction
