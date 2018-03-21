0aaca7bf34c94d7cbd762367e3a06927af72306a
ramadis <rramiro.o@hotmail.com>
2018-03-21 16:37:36 -0300

    Add data

--- TP/src/init.m
+++ TP/src/init.m
@@ -4,4 +4,3 @@ rand('state',sum(100*clock)); % seed random state
 configuration = config.parse(config.read('../config.json'));
 mlp = network.train(network.build(configuration));
 network.test(mlp)
-%mlp.train(parse.input('../input/train.json'));
