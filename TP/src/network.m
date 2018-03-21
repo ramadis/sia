a7da8a43e9a16a787ca586c3fa7f81d0b1d8d09a
ramadis <rramiro.o@hotmail.com>
2018-03-21 13:31:56 -0300

    Improve forward pass

--- TP/src/network.m
+++ TP/src/network.m
@@ -4,9 +4,14 @@ function retval = network
   retval.test = @test;
 endfunction
 
-function retval = setInitialWeights (weights, size) % TODO check function signature
+function retval = setInitialWeights (config) % TODO check function signature
+  config.weights{1} = 2 * rand(10,3) - 1;
+  config.weights{2} = 2 * rand(1,11) - 1;
+  retval = config.weights;
+  return;
+
   if (rows(weights) < 1)
-    retval = 2 * rand(10,3) - 1; % random values within [-1;1] (Filas de la current x Columnas de las previous)
+    retval = 2 * rand(10,3) - 1; % random values within [-1;1] (Filas de la current x Columnas de las previous) + el bias (que no lee weights pero si da weights)
     return
   endif
 
@@ -21,6 +26,7 @@ function retval = train (config)
     weights = input = output = [];
 
     for sample = data % Iterate through each sample (Lets do it stochasticly, why not)
+
       layer = config.layers(1);
       output{1} = [ layer.bias ; sample(2:end) ];
 
@@ -52,5 +58,5 @@ endfunction
 
 function retval = build (config)
   retval = config;
-  retval.weights{1} = setInitialWeights(config.weights);
+  retval.weights = setInitialWeights(config);
 endfunction
