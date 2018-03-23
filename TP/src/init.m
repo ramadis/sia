6cd2d1c54147c87d96b854be091f3050aa8bc406
ramadis <rramiro.o@hotmail.com>
2018-03-23 10:42:01 -0300

    One more todo

--- TP/src/init.m
+++ TP/src/init.m
@@ -1,6 +1,7 @@
 % TODO: Standarize data
 % TODO: Backprop optimizations
 % TODO: Load and test using the test set
+% TODO: Evaluate change input format
 
 addpath('../lib/jsonlab');
 rand('state', sum( 100 * clock )); % seed random state
