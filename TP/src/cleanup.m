d0f748c2f05a623786ea11215a594c1df7e7c583
romlatron <31899705+romlatron@users.noreply.github.com>
2018-03-25 12:57:35 -0300

    Update cleanup.m

--- TP/src/cleanup.m
+++ TP/src/cleanup.m
@@ -1,5 +1,6 @@
 % Clean up input script
 function retval = cleanup (input)
   data = loadjson(char(['../' input])).data;
-  retval = data;
+  %data = standardize(data);
+  retval = data;  
 endfunction
