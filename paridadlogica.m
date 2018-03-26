a00d04d6beef13c4d3da92e54f87e2d8d4bf676c
romlatron <31899705+romlatron@users.noreply.github.com>
2018-03-25 23:23:01 -0300

    Update paridadlogica.m

--- paridadlogica.m
+++ paridadlogica.m
@@ -1,13 +1,46 @@
-input = [0 0 0; 0 0 1; 0 1 0; 1 0 0; 0 1 1; 1 0 1; 1 1 0; 1 1 1];
 numIn = 3; #el numero de entradas
 numCapa1=16; #numero de neuronas en la capa intermedia
 
-for i = 1:8
-  desired_out(i)= mod((input(i, 1) + input (i, 2) + input(i, 3)), 2); #calculamos la paridad logica de cada entrada
+input=zeros(2^numIn, numIn);
+
+for i=2:2^numIn
+  j=1;
+  input(i, :)=input(i-1, :);
+  
+  if input(i-1, j) == 0
+    input(i, j)=1;
+  else
+    j=j+1;
+    while j<numIn+1
+        
+      if input(i-1, j) == 0;
+        input(i, j)=1;
+        
+        for k=j-1:-1:1  
+          input(i, k)=0;
+        end  
+        
+        break;
+      else
+       j=j+1;
+      endif
+
+    endwhile
+  endif  
+end  
+
+desired_out = zeros(2^numIn, 1);
+
+for i = 1:2^numIn
+  for j = 1:numIn
+    desired_out(i)+= input(i, j); #calculamos la paridad logica de cada entrada
+  end
+  desired_out(i) = mod(desired_out(i), 2);
 end
 
+desired_out
 
-biasOut = 1;
+biasOut =  -1*5.*rand(1, 1);
 biasCapa1 = -1*5.*rand(numCapa1, 1);
 coeff = 1; #coeficiente de aprentizaje
 rand('state',sum(100*clock));
@@ -16,10 +49,10 @@ weightsCapa1 = -1*2.*rand(numCapa1, 1);
 
 
 #entrenamiento
-iterations = 3000;
+iterations = 1000;
 for k=1:iterations
 
-for i = 1:8
+for i = 1:2^numIn
      [outCapa1, zCapa1]=computacionCapa(input(i, :).', weightsIn, biasCapa1); #ahi zCapa1 seria el resultado sin aplicar la funcion de salida
      [out, y]=computacionCapa(outCapa1, weightsCapa1, biasOut); #y y el resultado final sin aplicar la funcion
     
@@ -32,10 +65,12 @@ end
 end
 
 #?evaluacion
-out=zeros(8, 1);
-for i = 1:8
+out=zeros(2^numIn, 1);
+for i = 1:2^numIn
      outCapa1=computacionCapa(input(i, :).', weightsIn, biasCapa1);
      out(i)=computacionCapa(outCapa1, weightsCapa1, biasOut);
 end
 
 out
+
+error = (0.5*sum((desired_out - out) .^ 2)) / length(desired_out)
